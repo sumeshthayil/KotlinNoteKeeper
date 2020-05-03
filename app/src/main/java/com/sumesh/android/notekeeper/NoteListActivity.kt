35eacc95b7ab5a090871d5518bd745582af26495
Sumesh Anirudhan <sumeshta@gmail.com>
2020-05-03 22:43:19 +1200

    Populated note list with default data from data class DataManager

--- app/src/main/java/com/sumesh/android/notekeeper/NoteListActivity.kt
+++ app/src/main/java/com/sumesh/android/notekeeper/NoteListActivity.kt
@@ -2,10 +2,12 @@ package com.sumesh.android.notekeeper
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 
 import kotlinx.android.synthetic.main.activity_note_list.*
+import kotlinx.android.synthetic.main.content_note_list.*
 
 class   NoteListActivity : AppCompatActivity() {
 
@@ -18,6 +20,10 @@ class   NoteListActivity : AppCompatActivity() {
             var activityIntent = Intent(this, MainActivity::class.java)
             startActivity(activityIntent)
         }
+
+        listNotes.adapter = ArrayAdapter<NoteInfo>(this,
+                android.R.layout.simple_list_item_1,
+                DataManager.notes)
     }
 
 }
