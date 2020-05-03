111d917dcda3c7e6f13e6a887ef194e6f75e0f41
Sumesh Anirudhan <SabisMac@Sumeshs-Mini.fritz.box>
2020-05-03 13:38:14 +1200

    Initial commit

---
+++ app/src/main/java/com/sumesh/android/notekeeper/DataManager.kt
@@ -0,0 +1,25 @@
+package com.sumesh.android.notekeeper
+
+class DataManager {
+
+    val courses = HashMap<String, CourseInfo>()
+    val notes = ArrayList<NoteInfo>()
+
+    init {
+        initializeCourses()
+    }
+
+    private fun initializeCourses (){
+        var course = CourseInfo("android_intents","Android Programming with Intents")
+        courses.set(course.courseId, course)
+
+        course = CourseInfo("android_async","Android Async and Services")
+        courses.set(course.courseId, course)
+
+        course = CourseInfo(title = "Java Fundamentals",courseId = "java_lang")
+        courses.set(course.courseId, course)
+
+        course = CourseInfo("jave_core","Android Programming with Intents")
+        courses.set(course.courseId, course)
+    }
+}
---
+++ app/src/main/java/com/sumesh/android/notekeeper/NoteKeeperData.kt
@@ -0,0 +1,5 @@
+package com.sumesh.android.notekeeper
+
+class CourseInfo (val courseId: String, val title: String)
+
+class NoteInfo (var course: CourseInfo, var title: String, var text: String)
