91a730adec3e3f133c151044b1c12fc64c2c6ccb
Sumesh Anirudhan <sumeshta@gmail.com>
2020-05-03 19:10:40 +1200

    Added note list activity

---
+++ app/src/main/java/com/sumesh/android/notekeeper/NoteListActivity.kt
@@ -0,0 +1,23 @@
+package com.sumesh.android.notekeeper
+
+import android.content.Intent
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+
+import kotlinx.android.synthetic.main.activity_note_list.*
+
+class   NoteListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_note_list)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            var activityIntent = Intent(this, MainActivity::class.java)
+            startActivity(activityIntent)
+        }
+    }
+
+}
